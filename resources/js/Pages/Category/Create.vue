ab3a8e6c12b20604ea73758b13e860c2badd66c6
mohanadAbdallah <mohanad.abdalah50@gmail.com>
2025-07-22 13:46:45 +0300

    main First Commit

--- resources/js/Pages/Category/Create.vue
+++ resources/js/Pages/Category/Create.vue
@@ -40,7 +40,7 @@ const store = () => {
                                 </button>
                                 <Link :href="route('categories.index')"
                                       class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5">
-                                    Back To Categories
+                                    Cancel
                                 </Link>
                             </CategoryForm>
                         </div>
